0b118c7c5c495d5855e8f1f3e83d216d37123d78
unknown <ashybaye@PBB-S296-1.iowa.uiowa.edu>
2015-05-13 15:46:25 -0500

    Crushed JSON files

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyHTML = require('gulp-minify-html'),
+	minifyJSON = require('gulp-jsonminify'),
 	concat = require('gulp-concat');
 
 var env, 
@@ -77,7 +78,7 @@ gulp.task('watch', function() {
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 	gulp.watch('builds/development/*.html', ['html']);
-	gulp.watch(jsonSources, ['json']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('connect', function() {
@@ -95,7 +96,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-	gulp.src(jsonSources)
+	gulp.src('builds/development/js/*.json')
+		.pipe(gulpif(env === 'production', minifyJSON()))
+		.pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))	
 		.pipe(connect.reload())
 });
 
